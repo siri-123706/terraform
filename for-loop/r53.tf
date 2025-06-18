a4d6429c9dc3ba6fa312ef0952346bfeea7e8eb0
kodar@siri <shirishakodari35@.com>
2025-06-18 18:18:16 +0530

    terraform

--- for-loop/r53.tf
+++ for-loop/r53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "www" {
   for_each = aws_instance.roboshop
-  zone_id = var.zone_id
+  zone_id = var.zone_id 
   name    = "${each.key}.${var.domain_name}" 
   type    = "A"
   ttl     = 1
